3105cc5fa420b449a2d0c3694da1f4d8eb7cf2de
Diego Consen <diego.consen@navara.nl>
2024-06-27 10:23:05 +0200

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["typescript_guild_validation_*"],
+} satisfies Config;
